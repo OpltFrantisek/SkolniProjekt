d05760a7b553b07368abc05010fe1b388fda4381
František Oplt <opltfrantisek@gmail.com>
2015-12-12 13:33:49 +0100

    Ziskani_souboru_z_webu
    získá morseovu abecedu a ulozi jako string

---
+++ Morseovka/Morseovka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morseovka
+{
+    class Program
+    {
+        private static string id = "qlR4TYSw1U1VimB0TqhZq6jSXwFn0yt7"; // id našeho týmu
+        private static string web = "http://ioe.zcu.cz/morse.php?id="; 
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            string url = web + id;
+            string data = client.DownloadString(url);
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+
+    }
+}
